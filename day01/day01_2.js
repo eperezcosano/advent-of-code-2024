3656bb31651779de14db64ad1a6c5c1ecc9c62b0
Ethan <izanperez98@gmail.com>
2024-12-01 13:17:13 +0100

    day01 init

---
+++ day01/day01_2.js
@@ -0,0 +1,18 @@
+/*
+* --- Day X:  ---
+* --- Part X ---
+* Advent Of Code 2024
+* */
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./day01.txt')
+})
+
+lineReader.on('line', line => {
+})
+
+lineReader.on('close', () => {
+    // Result:
+})
+
+
