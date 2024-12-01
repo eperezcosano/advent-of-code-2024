5bc553d07d68cba0162ee3f07d2ae72792fe7898
Ethan <izanperez98@gmail.com>
2024-12-01 13:32:56 +0100

    day01 part one

--- day01/day01_1.js
+++ day01/day01_1.js
@@ -8,11 +8,18 @@ const lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('./day01.txt')
 })
 
+const [ left, right ] = [ [], [] ]
+
 lineReader.on('line', line => {
+    const [leftNum, rightNum ] = line.split('   ').map(Number)
+    left.push(leftNum)
+    right.push(rightNum)
 })
 
 lineReader.on('close', () => {
-    // Result:
+    left.sort((a, b) => a - b)
+    right.sort((a, b) => a - b)
+    const res = new Array(left.length).fill(0).reduce((acc, val, i) => acc + Math.abs(left[i] - right[i]), 0)
+    console.log('Result:', res)
+    // Result: 1666427
 })
-
-
