ea6484d799989795ec7051592d43ece639784146
Ethan <izanperez98@gmail.com>
2024-12-02 01:19:46 +0100

    day01 part one

--- day01/day01_1.js
+++ day01/day01_1.js
@@ -19,7 +19,7 @@ lineReader.on('line', line => {
 lineReader.on('close', () => {
     left.sort((a, b) => a - b)
     right.sort((a, b) => a - b)
-    const res = new Array(left.length).fill(0).reduce((acc, val, i) => acc + Math.abs(left[i] - right[i]), 0)
+    const res = left.reduce((acc, val, i) => acc + Math.abs(val - right[i]), 0)
     console.log('Result:', res)
     // Result: 1666427
 })
