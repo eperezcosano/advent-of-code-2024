04ffa14fea7a62b8118efaf5a0b8b338c6a50ab1
Izan Pérez Cosano <izanperez98@gmail.com>
2024-12-03 11:48:30 +0100

    day03 part one

--- day03/day03_1.js
+++ day03/day03_1.js
@@ -8,9 +8,15 @@ const lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('./day03.txt')
 })
 
-lineReader.on('line', line => {
-})
+let input = ""
+
+lineReader.on('line', line => input += line)
 
 lineReader.on('close', () => {
-    // Result:
+    const res = input
+        .match(/mul\(\d{1,3},\d{1,3}\)/g)
+        .map(mul => mul.match(/\d+/g))
+        .reduce((sum, nums) => sum + nums[0] * nums[1], 0)
+    console.log('Result:', res)
+    // Result: 170068701
 })
