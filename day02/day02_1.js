1d521cd5d3ab499ff322dd694b2a417f7d385f5e
Izan Pérez Cosano <izanperez98@gmail.com>
2024-12-03 10:48:01 +0100

    day02 part one

--- day02/day02_1.js
+++ day02/day02_1.js
@@ -1,16 +1,21 @@
 /*
-* --- Day X:  ---
-* --- Part X ---
-* Advent Of Code 2024
+* --- Day 2: Red-Nosed Reports ---
+*         --- Part One ---
+*       Advent Of Code 2024
 * */
 
 const lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('./day02.txt')
 })
 
+let res = 0
+
 lineReader.on('line', line => {
+    const arr = line.split(' ').map(Number)
+    res += arr.every((val, i) => i === 0 || Math.sign(val - arr[i - 1]) !== 0 && Math.sign(arr[1] - arr[0]) === Math.sign(val - arr[i - 1]) && Math.abs(val - arr[i - 1]) <= 3) ? 1 : 0
 })
 
 lineReader.on('close', () => {
-    // Result:
+    console.log('Result:', res)
+    // Result: 257
 })
