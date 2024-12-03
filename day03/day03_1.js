60966a16b2fb0990ae6009ccd9a2f024423e9129
Izan Pérez Cosano <izanperez98@gmail.com>
2024-12-03 11:08:11 +0100

    day03 init

---
+++ day03/day03_1.js
@@ -0,0 +1,16 @@
+/*
+* --- Day 3: Mull It Over ---
+*       --- Part One ---
+*      Advent Of Code 2024
+* */
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./day03.txt')
+})
+
+lineReader.on('line', line => {
+})
+
+lineReader.on('close', () => {
+    // Result:
+})
