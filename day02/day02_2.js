e4a737111b1ba4b99327083a410551306746047c
Izan Pérez Cosano <izanperez98@gmail.com>
2024-12-03 11:05:42 +0100

    day02 part two

--- day02/day02_2.js
+++ day02/day02_2.js
@@ -1,18 +1,30 @@
 /*
-* --- Day X:  ---
-* --- Part X ---
-* Advent Of Code 2024
+* --- Day 2: Red-Nosed Reports ---
+*         --- Part Two ---
+*       Advent Of Code 2024
 * */
 
 const lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('./day02.txt')
 })
 
-lineReader.on('line', line => {
-})
+const reports = []
+
+function isSafe(arr) {
+    return arr.every((val, i) => i === 0 || Math.sign(val - arr[i - 1]) !== 0 && Math.sign(arr[1] - arr[0]) === Math.sign(val - arr[i - 1]) && Math.abs(val - arr[i - 1]) <= 3)
+}
+
+lineReader.on('line', line => reports.push(line.split(' ').map(Number)))
 
 lineReader.on('close', () => {
-    // Result:
+    const res = reports.reduce((acc, report) => {
+        if (isSafe(report)) return ++acc
+        for (let i = 0; i < report.length; i++) {
+            const sub = [...report.slice(0, i), ...report.slice(i + 1)]
+            if (isSafe(sub)) return ++acc
+        }
+        return acc
+    }, 0)
+    console.log('Result:', res)
+    // Result: 328
 })
-
-
