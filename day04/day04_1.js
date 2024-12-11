4b3905febcd14bc6e1a8606a39faaaf2e4c4ae01
Izan Pérez Cosano <izanperez98@gmail.com>
2024-12-11 11:39:17 +0100

    day04 dev

--- day04/day04_1.js
+++ day04/day04_1.js
@@ -5,11 +5,13 @@
 * */
 
 const lineReader = require('readline').createInterface({
-    input: require('fs').createReadStream('./test.txt')
+    input: require('fs').createReadStream('./day04.txt')
 })
 
 const word = "XMAS"
 const grid = []
+
+// Too low 2492
 
 function findWordInLine(arr) {
     let count = 0
@@ -38,7 +40,7 @@ function findHorizontal() {
 function findVertical() {
     let count = 0
     for (let x = 0; x < grid[0].length; x++) {
-        let arr = []
+        const arr = []
         for (let y = 0; y < grid.length; y++) {
             arr.push(grid[y][x])
         }
@@ -49,8 +51,38 @@ function findVertical() {
 
 function findDiagonalDesc() {
     let count = 0
-    for (let y = 0; y < grid.length; y++) {
+    for (let x = 0; x <= grid[0].length - word.length; x++) {
+        const arr = []
+        for (let d = 0; d + x < grid[0].length; d++) {
+            arr.push(grid[d][d + x])
+        }
+        count += findWordInLine(arr)
+    }
+    for (let y = 1; y <= grid.length - word.length; y++) {
+        let arr = []
+        for (let d = 0; d + y < grid.length; d++) {
+            arr.push(grid[d + y][d])
+        }
+        count += findWordInLine(arr)
+    }
+    return count
+}
 
+function findDiagonalAsc() {
+    let count = 0
+    for (let x = grid[0].length - 1; x >= word.length - 1; x--) {
+        const arr = []
+        for (let d = 0; x - d >= 0; d++) {
+            arr.push(grid[d][x - d])
+        }
+        count += findWordInLine(arr)
+    }
+    for (let y = 1; y <= grid.length - word.length; y++) {
+        let arr = []
+        for (let d = 0; d + y < grid.length; d++) {
+            arr.push(grid[d + y][grid[0].length - 1 - d])
+        }
+        count += findWordInLine(arr)
     }
     return count
 }
@@ -58,6 +90,7 @@ function findDiagonalDesc() {
 lineReader.on('line', line => grid.push(line.split('')))
 
 lineReader.on('close', () => {
-    findVertical()
+    const res = findHorizontal() + findVertical() + findDiagonalDesc() + findDiagonalDesc()
+    console.log('Result:', res)
     // Result:
 })
