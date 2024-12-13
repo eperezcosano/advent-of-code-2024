e957388e1d19df62447ef372caad371a513fb347
Izan Pérez Cosano <izanperez98@gmail.com>
2024-12-13 19:29:47 +0100

    day06 init

---
+++ day06/day06_1.js
@@ -0,0 +1,16 @@
+/*
+*       --- Day 6: Guard Gallivant ---
+*              --- Part One ---
+*             Advent Of Code 2024
+* */
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./day06.txt')
+})
+
+lineReader.on('line', line => {
+})
+
+lineReader.on('close', () => {
+    // Result:
+})
