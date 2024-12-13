f166b6c3e6b4a13809a99dc7ef3ff7d3f507a522
Izan Pérez Cosano <izanperez98@gmail.com>
2024-12-13 18:57:55 +0100

    day05 part one

--- day05/day05_1.js
+++ day05/day05_1.js
@@ -1,16 +1,39 @@
 /*
-* --- Day X:  ---
-* --- Part X ---
-* Advent Of Code 2024
+*       --- Day 5: Print Queue ---
+*           --- Part One ---
+*          Advent Of Code 2024
 * */
 
 const lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('./day05.txt')
 })
 
+const rules = new Map()
+const updates = []
+
+function isCorrectOrder(arr) {
+    const tmp = arr.slice()
+    tmp.sort((a, b) => {
+        const vals = rules.get(a)
+        if (vals && vals.includes(b)) return -1
+        return 0
+    })
+    return arr.every((val, i) => val === tmp[i])
+}
+
+let breakLine = false
 lineReader.on('line', line => {
+    if (line.length === 0) breakLine = true
+    else if (!breakLine) {
+        const [key, val] = line.split('|').map(Number)
+        rules.has(key) ? rules.set(key, [...rules.get(key), val]) : rules.set(key, [val])
+    } else {
+        updates.push(line.split(',').map(Number))
+    }
 })
 
 lineReader.on('close', () => {
-    // Result:
+    const res = updates.filter(update => isCorrectOrder(update)).reduce((acc, arr) => acc + arr[Math.floor(arr.length / 2)], 0)
+    console.log('Result:', res)
+    // Result: 4689
 })
