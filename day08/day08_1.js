11bdd580b6ec81df21ecfcf07fc88ed368919e37
Ethan <izanperez98@gmail.com>
2025-01-28 17:55:55 +0100

    day08 init

---
+++ day08/day08_1.js
@@ -0,0 +1,16 @@
+/*
+* --- Day 8: Resonant Collinearity ---
+*           --- Part One ---
+*          Advent Of Code 2024
+* */
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./day08.txt')
+})
+
+lineReader.on('line', line => {
+})
+
+lineReader.on('close', () => {
+    // Result:
+})
