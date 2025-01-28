04f582f0a5c9f25359a222bae7bb94376b04f9f9
Ethan <izanperez98@gmail.com>
2025-01-28 18:12:23 +0100

    day08 part one dev

--- day08/day08_1.js
+++ day08/day08_1.js
@@ -5,12 +5,30 @@
 * */
 
 const lineReader = require('readline').createInterface({
-    input: require('fs').createReadStream('./day08.txt')
+    input: require('fs').createReadStream('./test.txt')
 })
 
-lineReader.on('line', line => {
-})
+const grid = []
+const antennas = new Map()
+
+function printGrid() {
+    grid.forEach(row => console.log(row.join('')))
+}
+
+function getAntennas() {
+    for (let y = 0; y < grid.length; y++) {
+        for (let x = 0; x < grid[y].length; x++) {
+            if (grid[y][x] === '.') continue
+            antennas.has(grid[y][x]) ? antennas.set(grid[y][x], [...antennas.get(grid[y][x]), [y, x]]) : antennas.set(grid[y][x], [[y, x]])
+        }
+    }
+}
+
+lineReader.on('line', line => grid.push(line.split('')))
 
 lineReader.on('close', () => {
+    //printGrid()
+    getAntennas()
+    console.log(antennas)
     // Result:
 })
