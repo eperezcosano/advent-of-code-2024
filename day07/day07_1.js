60bc3360e87f3960f6efe7c5b1911a86efae972a
Ethan <izanperez98@gmail.com>
2025-01-28 12:18:54 +0100

    day07 init

---
+++ day07/day07_1.js
@@ -0,0 +1,16 @@
+/*
+* --- Day 7: Bridge Repair ---
+*       --- Part One ---
+*      Advent Of Code 2024
+* */
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./day07.txt')
+})
+
+lineReader.on('line', line => {
+})
+
+lineReader.on('close', () => {
+    // Result:
+})
