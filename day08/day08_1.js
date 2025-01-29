9d3da2ca465e4f72a9aaa2043c886342b12801c4
Ethan <izanperez98@gmail.com>
2025-01-29 12:12:47 +0100

    day08 part one

--- day08/day08_1.js
+++ day08/day08_1.js
@@ -5,14 +5,23 @@
 * */
 
 const lineReader = require('readline').createInterface({
-    input: require('fs').createReadStream('./test.txt')
+    input: require('fs').createReadStream('./day08.txt')
 })
 
 const grid = []
 const antennas = new Map()
+const antinodes = new Set()
 
-function printGrid() {
-    grid.forEach(row => console.log(row.join('')))
+function getPairs(points) {
+    for (let i = 0; i < points.length; i++) {
+        for (let j = i + 1; j < points.length; j++) {
+            const [[y1, x1], [y2, x2]] = [points[i], points[j]]
+            const [dy, dx, sy, sx] = [Math.abs(y1 - y2), Math.abs(x1 - x2), Math.sign(y1 - y2), Math.sign(x1 - x2)]
+            const [ay, ax, by, bx] = [y1 + sy * dy, x1 + sx * dx, y2 - sy * dy, x2 - sx * dx]
+            if (Math.min(ay, ax) >= 0 && ay < grid.length && ax < grid[0].length) antinodes.add([ay, ax].join())
+            if (Math.min(by, bx) >= 0 && by < grid.length && bx < grid[0].length) antinodes.add([by, bx].join())
+        }
+    }
 }
 
 function getAntennas() {
@@ -27,8 +36,9 @@ function getAntennas() {
 lineReader.on('line', line => grid.push(line.split('')))
 
 lineReader.on('close', () => {
-    //printGrid()
     getAntennas()
-    console.log(antennas)
-    // Result:
+    antennas.forEach(antenna => getPairs(antenna))
+    const res = antinodes.size
+    console.log('Result:', res)
+    // Result: 313
 })
--- day08/day08_2.js
+++ day08/day08_2.js
@@ -1,16 +1,44 @@
 /*
-* --- Day X:  ---
-* --- Part X ---
-* Advent Of Code 2024
+* --- Day 8: Resonant Collinearity ---
+*           --- Part Two ---
+*          Advent Of Code 2024
 * */
 
 const lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('./day08.txt')
 })
 
-lineReader.on('line', line => {
-})
+const grid = []
+const antennas = new Map()
+const antinodes = new Set()
+
+function getPairs(points) {
+    for (let i = 0; i < points.length; i++) {
+        for (let j = i + 1; j < points.length; j++) {
+            const [[y1, x1], [y2, x2]] = [points[i], points[j]]
+            const [dy, dx, sy, sx] = [Math.abs(y1 - y2), Math.abs(x1 - x2), Math.sign(y1 - y2), Math.sign(x1 - x2)]
+            const [ay, ax, by, bx] = [y1 + sy * dy, x1 + sx * dx, y2 - sy * dy, x2 - sx * dx]
+            if (Math.min(ay, ax) >= 0 && ay < grid.length && ax < grid[0].length) antinodes.add([ay, ax].join())
+            if (Math.min(by, bx) >= 0 && by < grid.length && bx < grid[0].length) antinodes.add([by, bx].join())
+        }
+    }
+}
+
+function getAntennas() {
+    for (let y = 0; y < grid.length; y++) {
+        for (let x = 0; x < grid[y].length; x++) {
+            if (grid[y][x] === '.') continue
+            antennas.has(grid[y][x]) ? antennas.set(grid[y][x], [...antennas.get(grid[y][x]), [y, x]]) : antennas.set(grid[y][x], [[y, x]])
+        }
+    }
+}
+
+lineReader.on('line', line => grid.push(line.split('')))
 
 lineReader.on('close', () => {
+    getAntennas()
+    antennas.forEach(antenna => getPairs(antenna))
+    const res = antinodes.size
+    console.log('Result:', res)
     // Result:
 })
