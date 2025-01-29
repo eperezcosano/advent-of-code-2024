a39ebd5eae039c7e4589c03a89cae22d09f3c8b4
Ethan <izanperez98@gmail.com>
2025-01-29 14:12:43 +0100

    day09 part one dev

--- day09/day09_1.js
+++ day09/day09_1.js
@@ -8,9 +8,34 @@ const lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('./day09.txt')
 })
 
-lineReader.on('line', line => {
-})
+let disk
+
+function compactBlocks(blocks) {
+    while (blocks.includes('.')) {
+        while (blocks[blocks.length - 1] === '.') blocks.pop()
+        const i = blocks.findIndex(elem => elem === '.')
+        blocks[i] = blocks.pop()
+    }
+    return blocks.map(Number)
+}
+
+function computeBlocks() {
+    let blocks = ""
+    for (let i = 0, id = 0; i < disk.length; i++) {
+        const n = parseInt(disk[i])
+        const s = (i % 2 === 0) ? `${id++}` : '.'
+        blocks += s.repeat(n)
+    }
+    return blocks.split('')
+}
+
+lineReader.on('line', line => disk = line)
 
 lineReader.on('close', () => {
+    const blocks = computeBlocks()
+    const compact = compactBlocks(blocks)
+    const res = compact.reduce((acc, num, i) => acc + i * num, 0)
+    console.log('Result', res)
     // Result:
 })
+// too low 89425419840
