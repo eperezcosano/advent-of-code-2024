fb215230bb075ded579063e85ef6b544b9c5340c
Ethan <izanperez98@gmail.com>
2025-02-19 15:42:44 +0100

    day10 init

---
+++ day10/day10_1.js
@@ -0,0 +1,16 @@
+/*
+* --- Day 10: Hoof It ---
+*     --- Part One ---
+*    Advent Of Code 2024
+* */
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./day10.txt')
+})
+
+lineReader.on('line', line => {
+})
+
+lineReader.on('close', () => {
+    // Result:
+})
