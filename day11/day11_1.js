d383a22d5bd659e4fdd4049c5db5190166080f99
Ethan <izanperez98@gmail.com>
2025-08-18 12:32:29 +0200

    day11 part one

--- day11/day11_1.js
+++ day11/day11_1.js
@@ -8,9 +8,28 @@ const lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('./day11.txt')
 })
 
-lineReader.on('line', line => {
-})
+const stones = []
+const states = new Map()
+
+function blink() {
+    const init = stones.join()
+    if (states.has(init)) return states.get(init).split(',').map(Number)
+    for (let i = 0; i < stones.length; i++) {
+        if (stones[i] === 0) stones[i] = 1
+        else if (stones[i].toString().length % 2 === 0) {
+            const str = stones[i].toString()
+            const [left, right] = [str.slice(0, str.length / 2), str.slice(str.length / 2)].map(Number)
+            stones[i] = left
+            stones.splice(++i, 0, right)
+        } else stones[i] *= 2024
+    }
+    states.set(init, stones.join())
+}
+
+lineReader.on('line', line => stones.push(...line.split(' ').map(Number)))
 
 lineReader.on('close', () => {
-    // Result:
+    for (let n = 0; n < 25; n++) blink()
+    console.log('Result:', stones.length)
+    // Result: 213625
 })
