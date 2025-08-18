a7437bc3cc61e6c8bd7b22149c73f8243b41fa49
Ethan <izanperez98@gmail.com>
2025-08-18 12:22:42 +0200

    day11 init

---
+++ day11/day11_1.js
@@ -0,0 +1,16 @@
+/*
+* --- Day 11: Plutonian Pebbles ---
+*         --- Part One ---
+*        Advent Of Code 2024
+* */
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./day11.txt')
+})
+
+lineReader.on('line', line => {
+})
+
+lineReader.on('close', () => {
+    // Result:
+})
