c87ad663bb8c906548cf8a205a4e5ecda50d9f7e
Ethan <izanperez98@gmail.com>
2025-09-10 13:09:21 +0200

    day14 part two

--- day14/day14_2.js
+++ day14/day14_2.js
@@ -5,35 +5,29 @@
 * */
 
 const lineReader = require('readline').createInterface({
-    input: require('fs').createReadStream('./test.txt')
+    input: require('fs').createReadStream('./day14.txt')
 })
 
-const [cols, rows] = [11, 7]
+const [cols, rows] = [101, 103]
 const robots = []
 
 function mod(n, m) {
     return ((n % m) + m) % m
 }
 
-function move() {
+function move(s) {
     for (let i = 0; i < robots.length; i++) {
         const [[px, py], [vx, vy]] = robots[i]
-        const x = px + vx * 100
-        const y = py + vy * 100
+        const x = px + vx * s
+        const y = py + vy * s
         robots[i] = [[mod(x, cols), mod(y, rows)], [vx, vy]]
     }
 }
 
-function safetyFactor() {
-    const quadrants = [0, 0, 0, 0]
-    const [y, x] = [(rows - 1) / 2, (cols - 1) / 2]
-    for (const [[px, py]] of robots) {
-        if (px < x && py < y) quadrants[0]++
-        else if (px > x && py < y) quadrants[1]++
-        else if (px < x && py > y) quadrants[2]++
-        else if (px > x && py > y) quadrants[3]++
-    }
-    return quadrants.reduce((acc, val) => acc * val, 1)
+function testChristmassTree() {
+    const grid = Array(rows).fill(false).map(() => new Array(cols).fill(' '))
+    for (const [[px, py]] of robots) grid[py][px] = '*'
+    return grid.some(row => row.join('').includes('*********'))
 }
 
 lineReader.on('line', line => {
@@ -42,23 +36,12 @@ lineReader.on('line', line => {
 })
 
 lineReader.on('close', () => {
-    move()
-    const res = safetyFactor()
-    console.log('Result:', res)
-    // Result:
-})
-
-function printGrid() {
-    const grid = Array(rows).fill(false).map(() => new Array(cols).fill('.'))
-    for (const [[px, py]] of robots) {
-        if (grid[py][px] === '.') grid[py][px] = 1
-        else grid[py][px]++
-    }
-    grid.forEach(row => console.log(row.join('')))
-    for (let y = 0; y < grid.length; y++) {
-        for (let x = 0; x < grid[0].length; x++) {
-            if (y === (rows - 1) / 2 || x === (cols - 1) / 2) grid[y][x] = ' '
+    for (let res = 1; res <= 1e5; res++) {
+        move(1)
+        if (testChristmassTree()) {
+            console.log('Result:', res)
+            break
         }
     }
-    grid.forEach(row => console.log(row.join('')))
-}
+    // Result: 6876
+})
