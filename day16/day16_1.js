e9e8d0051d9d2f4c15278becb28a40f7c7d895bd
Ethan <izanperez98@gmail.com>
2025-09-15 17:44:24 +0200

    day16 init

---
+++ day16/day16_1.js
@@ -0,0 +1,16 @@
+/*
+* --- Day 16: Reindeer Maze ---
+*       --- Part One ---
+*      Advent Of Code 2024
+* */
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./day16.txt')
+})
+
+lineReader.on('line', line => {
+})
+
+lineReader.on('close', () => {
+    // Result:
+})
